d3a1677edaa423aedd88cdb8d1c4e1997e12213c
fairydust <rtyh12@gmail.com>
2024-05-23 15:22:14 +0000

    Add atuin fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,4 +2,5 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     # starship init fish | source
     #zoxide init fish | source
+    atuin init --disable-up-arrow fish | source
 end
