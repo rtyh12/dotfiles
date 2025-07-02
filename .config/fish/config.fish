725957e40c0bb3d7b64daafb3a3b876e210375ce
Mihai Verzan <Mihai.Verzan@ibm.com>
2025-07-02 13:32:32 +0300

    Add fzf to config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,5 +2,8 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     # starship init fish | source
     #zoxide init fish | source
-    atuin init --disable-up-arrow fish | source
+    # atuin init --disable-up-arrow fish | source
+
+    # Set up fzf key bindings
+    fzf --fish | source
 end
